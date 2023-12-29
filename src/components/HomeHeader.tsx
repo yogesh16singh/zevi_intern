9d103a82d29c8ef963372fcf9b634c190fb222b7
yogesh16singh <ys1997642@gmail.com>
2023-12-29 18:42:04 +0530

    home page done

---
+++ src/components/HomeHeader.tsx
@@ -0,0 +1,11 @@
+import zevi_logo from "../themes/SVGs/zevi_logo_svg.svg";
+import "./HomeHeader.scss";
+const HomeHeader = () => {
+    return (
+        <div className="home_header">
+            <img src={zevi_logo} alt="" />
+        </div>
+    );
+};
+
+export default HomeHeader;
